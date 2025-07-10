6824feb896b5e623d1fc9bbaea003be3653c0a78
Radsproject01 <radhikakul01@gmail.com>
2025-07-10 17:31:38 +0530

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn_go = New System.Windows.Forms.Button()
+        Me.TxtFirstname = New System.Windows.Forms.TextBox()
+        Me.TxtGender = New System.Windows.Forms.TextBox()
+        Me.TxtLastname = New System.Windows.Forms.TextBox()
+        Me.LblFirstname = New System.Windows.Forms.Label()
+        Me.LblLastname = New System.Windows.Forms.Label()
+        Me.LblGender = New System.Windows.Forms.Label()
+        Me.LstOccupations = New System.Windows.Forms.ListBox()
+        Me.Occupations = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Btn_go
+        '
+        Me.Btn_go.Location = New System.Drawing.Point(356, 357)
+        Me.Btn_go.Name = "Btn_go"
+        Me.Btn_go.Size = New System.Drawing.Size(176, 81)
+        Me.Btn_go.TabIndex = 0
+        Me.Btn_go.Text = "Go"
+        Me.Btn_go.UseVisualStyleBackColor = True
+        '
+        'TxtFirstname
+        '
+        Me.TxtFirstname.Location = New System.Drawing.Point(356, 58)
+        Me.TxtFirstname.Name = "TxtFirstname"
+        Me.TxtFirstname.Size = New System.Drawing.Size(161, 22)
+        Me.TxtFirstname.TabIndex = 1
+        '
+        'TxtGender
+        '
+        Me.TxtGender.Location = New System.Drawing.Point(356, 114)
+        Me.TxtGender.Name = "TxtGender"
+        Me.TxtGender.Size = New System.Drawing.Size(161, 22)
+        Me.TxtGender.TabIndex = 2
+        '
+        'TxtLastname
+        '
+        Me.TxtLastname.Location = New System.Drawing.Point(356, 86)
+        Me.TxtLastname.Name = "TxtLastname"
+        Me.TxtLastname.Size = New System.Drawing.Size(161, 22)
+        Me.TxtLastname.TabIndex = 3
+        '
+        'LblFirstname
+        '
+        Me.LblFirstname.AutoSize = True
+        Me.LblFirstname.Location = New System.Drawing.Point(278, 61)
+        Me.LblFirstname.Name = "LblFirstname"
+        Me.LblFirstname.Size = New System.Drawing.Size(72, 16)
+        Me.LblFirstname.TabIndex = 4
+        Me.LblFirstname.Text = "First Name"
+        '
+        'LblLastname
+        '
+        Me.LblLastname.AutoSize = True
+        Me.LblLastname.Location = New System.Drawing.Point(278, 92)
+        Me.LblLastname.Name = "LblLastname"
+        Me.LblLastname.Size = New System.Drawing.Size(72, 16)
+        Me.LblLastname.TabIndex = 5
+        Me.LblLastname.Text = "Last Name"
+        '
+        'LblGender
+        '
+        Me.LblGender.AutoSize = True
+        Me.LblGender.Location = New System.Drawing.Point(298, 120)
+        Me.LblGender.Name = "LblGender"
+        Me.LblGender.Size = New System.Drawing.Size(52, 16)
+        Me.LblGender.TabIndex = 6
+        Me.LblGender.Text = "Gender"
+        '
+        'LstOccupations
+        '
+        Me.LstOccupations.FormattingEnabled = True
+        Me.LstOccupations.ItemHeight = 16
+        Me.LstOccupations.Items.AddRange(New Object() {"Actor", "Singer", "Broadway", "Teacher", "Driver"})
+        Me.LstOccupations.Location = New System.Drawing.Point(356, 153)
+        Me.LstOccupations.Name = "LstOccupations"
+        Me.LstOccupations.Size = New System.Drawing.Size(160, 196)
+        Me.LstOccupations.TabIndex = 7
+        '
+        'Occupations
+        '
+        Me.Occupations.AutoSize = True
+        Me.Occupations.Location = New System.Drawing.Point(268, 219)
+        Me.Occupations.Name = "Occupations"
+        Me.Occupations.Size = New System.Drawing.Size(82, 16)
+        Me.Occupations.TabIndex = 8
+        Me.Occupations.Text = "Occupations"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Occupations)
+        Me.Controls.Add(Me.LstOccupations)
+        Me.Controls.Add(Me.LblGender)
+        Me.Controls.Add(Me.LblLastname)
+        Me.Controls.Add(Me.LblFirstname)
+        Me.Controls.Add(Me.TxtLastname)
+        Me.Controls.Add(Me.TxtGender)
+        Me.Controls.Add(Me.TxtFirstname)
+        Me.Controls.Add(Me.Btn_go)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btn_go As Button
+    Friend WithEvents TxtFirstname As TextBox
+    Friend WithEvents TxtGender As TextBox
+    Friend WithEvents TxtLastname As TextBox
+    Friend WithEvents LblFirstname As Label
+    Friend WithEvents LblLastname As Label
+    Friend WithEvents LblGender As Label
+    Friend WithEvents LstOccupations As ListBox
+    Friend WithEvents Occupations As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LstOccupations.Items.Add("Doctor")
+        LstOccupations.Items.Add("Engineer")
+        LstOccupations.Items.Add("Teacher")
+        LstOccupations.Items.Add("Artist")
+
+    End Sub
+
+    Private Sub Btn_go_Click(sender As Object, e As EventArgs) Handles Btn_go.Click
+        Dim stFirstnm As String
+        Dim stLastnm As String
+        Dim stOccupation As String
+        Dim stGender As String
+
+        stFirstnm = TxtFirstName.Text
+        stLastnm = TxtLastName.Text
+        stGender = TxtGender.Text
+        stOccupation = LstOccupations.SelectedItem.ToString()
+        If LstOccupations.SelectedItem IsNot Nothing Then
+            stOccupation = LstOccupations.SelectedItem.ToString()
+        Else
+            stOccupation = "Not selected"
+        End If
+
+        MsgBox("Hi " & stFirstnm & " " & stLastnm & " You are a " & stGender & " " & stOccupation & ".")
+
+
+    End Sub
+End Class
